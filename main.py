6b6d53878a314873954b95f0d3210bb6918da0ee
roni-boker <roni.boker@yandex.com>
2024-01-16 10:48:51 +0200

    add tensorboard and argparse

--- main.py
+++ main.py
@@ -11,9 +11,40 @@ import cv2
 import time
 import os
 import copy
+import matplotlib.pyplot as plt
+import argparse
+
+# # office pc
+# DATA_DIR = '/home/user/Datasets'  
+# RUNS_DIR = '/home/user/Projects/runs/pytorch-ex2'
+# laptop:
+DATA_DIR = '/home/roni/Projects/Data'  
+RUNS_DIR = '/home/roni/Projects/runs/pytorch-ex2'
+
+
+
+from torch.utils.tensorboard import SummaryWriter
+tb_writer = SummaryWriter(RUNS_DIR)
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def get_args_parser(add_help=True):
+    parser = argparse.ArgumentParser(description='Pytorch-ex2 Training', add_help=add_help)
+    parser.add_argument('--data-path', default=DATA_DIR, type=str, help='path of dataset')
+    parser.add_argument('--output-dir', default=RUNS_DIR, type=str, help='path to save outputs')
+    parser.add_argument('--name', default='exp', type=str, help='experiment name, saved to output_dir/name')
+    parser.add_argument('--batch-size', default=32, type=int, help='total batch size for all GPUs')
+    parser.add_argument('--epochs', default=1000, type=int, help='number of total epochs to run')
+    parser.add_argument('--workers', default=8, type=int, help='number of data loading workers (default: 8)')
+    parser.add_argument('--device', default='0', type=str, help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--eval-interval', default=20, type=int, help='evaluate at every interval epochs')
+    parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume the most recent training')
+    parser.add_argument('--write_trainbatch_tb', action='store_true', help='write train_batch image to tensorboard once an epoch, may slightly slower train speed if open')
+    parser.add_argument('--run_debugpy', nargs='?', const=True, default=False, help='run debugpy service to enable debugging through VS-Code')
+    return parser
+
 
 
 def get_data_loaders(data_dir:str) -> tuple[DataLoader, DataLoader, DataLoader, list[str]]: 
@@ -55,7 +86,8 @@ def get_data_loaders(data_dir:str) -> tuple[DataLoader, DataLoader, DataLoader, 
     return dataloaders, class_names
 
 
-def train_loop(model:nn.Module, criterion:nn.Module, dataloaders:dict[DataLoader], optimizer:torch.optim.Optimizer, scheduler=None, num_epochs:int=3):
+def train_loop(model:nn.Module, criterion:nn.Module, dataloaders:dict[DataLoader], optimizer:torch.optim.Optimizer, 
+               scheduler=None, num_epochs:int=3):
 
     best_model_weights = copy.deepcopy(model.state_dict())
     best_acc = 0.0
@@ -77,9 +109,10 @@ def train_loop(model:nn.Module, criterion:nn.Module, dataloaders:dict[DataLoader
         
             running_loss = 0.0
             running_correct = 0
+            n_total_steps = len(dataloaders[phase])
 
             #iterate over the data:
-            for inputs, labels in dataloaders[phase]:
+            for i, (inputs, labels) in enumerate(dataloaders[phase]):
                 inputs = inputs.to(device)
                 labels = labels.to(device)
 
@@ -98,7 +131,11 @@ def train_loop(model:nn.Module, criterion:nn.Module, dataloaders:dict[DataLoader
                 #statistics:
                 running_loss += loss.item() * inputs.size(0)
                 running_correct += torch.sum(pred==labels.data)
-
+                iter_loss = loss.item()
+                iter_accuracy = torch.sum(pred==labels.data).double() / len(labels)
+                tb_writer.add_scalar('training loss', iter_loss, epoch * n_total_steps + i)
+                tb_writer.add_scalar('accuracy', iter_accuracy, epoch * n_total_steps + i)
+            
             if phase=='train' and scheduler:
                 scheduler.step()
 
@@ -158,21 +195,39 @@ class MyConvNet(nn.Module):
         return x
 
 
+def print_grid_images(example_data):
+    
+    for i in range(6):
+        plt.subplot(2, 3, i+1)
+        plt.imshow(example_data[i][0], cmap='gray')
+    plt.show()
+
+
 
 if __name__ == '__main__':
 
     print(f'device = {device}')
 
-    data_dir = '/home/user/Datasets'
+    args = get_args_parser().parse_args()
+    
     learning_rate = 0.01
 
-    dataloaders, class_names = get_data_loaders(data_dir)
+    dataloaders, class_names = get_data_loaders(args.data_path)
     print(f'classes = {class_names}')
+    example_data, _ = iter(dataloaders['test']).next()
+    # print_grid_images(example_data)
+    image_grid = torchvision.utils.make_grid(example_data)
+    tb_writer.add_image('MNIST images', image_grid)
+    tb_writer.flush()
 
     model = MyConvNet().to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-    train_loop(model, dataloaders=dataloaders, criterion=criterion, optimizer=optimizer)
+    tb_writer.add_graph(model, example_data.to(device))
+    tb_writer.flush()
 
+    train_loop(model, dataloaders=dataloaders, criterion=criterion, optimizer=optimizer, num_epochs=args.epochs)
+
+    tb_writer.close()
     print('done.')
